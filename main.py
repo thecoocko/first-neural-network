99fe19a35632fd8439cb4fd76d0489306f672fae
Coocko <krabik4794@gmail.com>
2021-12-25 13:12:06 +0200

    confussion matrix not implemented

--- main.py
+++ main.py
@@ -25,6 +25,14 @@ def getTrainLabels():
     assert min(labels) == 0 and max(labels) == 9
     assert len(labels) == 60000
 
+    labels_np = np.array(labels).reshape((-1,1))
+    encoder = OneHotEncoder(categories='auto')
+    labels_np_onehot =  encoder.fit_transform(labels_np).toarray()
+
+    return labels_np_onehot
+
+
+
 
 def getTrainImages(images,SIZE_OF_ONE_IMAGE):
     with gzip.open('train-images-idx3-ubyte.gz') as train_images:
@@ -46,9 +54,40 @@ def plotImage(pixels:np.array):
     plt.show()
 
 
+def trainingCheck(imges,labels_np_onehot):
+    x_train, x_test, y_train, y_test = train_test_split(images, labels_np_onehot)
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(input_shape=(SIZE_OF_ONE_IMAGE,), units = 128, activation='relu'))
+    model.add(keras.layers.Dense(10,activation='softmax'))
+    
+    model.summary()
+    
+    model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])
+    model.fit(np.array(x_train),np.array(y_train),epochs=20, batch_size=128)
+    print(model.evaluate(np.array(x_test),np.array(y_test)))
+
+    predicted_results = model.predict(x_test[1010].reshape((1, -1)))
+    print(predicted_results)
+
+    # predicted_outputs = np.argmax(model.predict(x_test),axis=1)
+    # expected_outputs = np.argmax(y_test, axis=1)
+
+    # predicted_confusion_matrix = confusion_matrix(expected_outputs,predicted_outputs)
+    # print(f"matrix {predicted_confusion_matrix}")
+    
+
+
+def networkTrain(SIZE_OF_ONE_IMAGE,x_sample, y_sample):
+    
+    pass
+
+
 if __name__ == '__main__':
     SIZE_OF_ONE_IMAGE = 28**2
     images = []
-    getTrainLabels()
+    print(getTrainLabels())
     getTrainImages(images,SIZE_OF_ONE_IMAGE)
-    plotImage(images[24])
+    print(trainingCheck(images,getTrainLabels()))
+    
+
+    plotImage(images[1010])
